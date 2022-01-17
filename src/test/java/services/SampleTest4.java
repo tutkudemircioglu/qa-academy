4fd5cef082a4502b541758b25a44d8f092b70678
tutkud <tutku.demircioglu@getir.com>
2022-01-17 21:31:22 +0300

    fix allure

--- src/test/java/services/SampleTest4.java
+++ src/test/java/services/SampleTest4.java
@@ -1,15 +1,12 @@
 package services;
 
-import io.qameta.allure.Attachment;
+import io.qameta.allure.Allure;
 import io.restassured.RestAssured;
 import io.restassured.internal.RequestSpecificationImpl;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.lessThan;
 
 public class SampleTest4 {
 
@@ -18,35 +15,21 @@ public class SampleTest4 {
     public void sample() {
         String baseUrl = "https://petstore.swagger.io/v2/store/inventory";
         RequestSpecification restAssuredReq = RestAssured.given()
+                .header("Study","Test")
                 .log()
                 .all(true);
         Response response = restAssuredReq.get(baseUrl);
-        attachment(restAssuredReq,baseUrl,response);
-        System.out.println("getBody: " + response.asString());
-        System.out.println("getBody: " + response.getBody().asString());
-        System.out.println("getStatusCode: " + response.getStatusCode());
-        System.out.println("getContent-Type: " + response.getHeader("Content-Type"));
-        System.out.println("getHeaders: " + response.getHeaders());
-        System.out.println("getTime: " + response.getTime());
-
+        attachment(restAssuredReq, baseUrl, response);
         Assert.assertEquals(response.getStatusCode(), 200);
     }
 
-    @Test
-    public void getInventory() {
-        given()
-                .log().all().
-                when()
-                .get("https://petstore.swagger.io/v2/store/inventory").
-                then()
-                .statusCode(200)
-                .time(lessThan(2000L))
-                .log().all();
-    }
 
-    @Attachment("https://petstore.swagger.io/v2/store/inventory")
-    public String attachment(RequestSpecification httpRequest, String path, Response response) {
-        String html = "Url = " + ((RequestSpecificationImpl) httpRequest).getBaseUri() + path + "\n \n" + "Request Headers = " + ((RequestSpecificationImpl) httpRequest).getHeaders() + "\n \n" + "Request Body = " + ((RequestSpecificationImpl) httpRequest).getBody() + "\n \n" + "Response Body = " + response.getBody().asString();
+    public String attachment(RequestSpecification httpRequest, String baseUrl, Response response) {
+        String html = "Url = " + baseUrl + "\n \n" +
+                "Request Headers = " + ((RequestSpecificationImpl) httpRequest).getHeaders() + "\n \n" +
+                "Request Body = " + ((RequestSpecificationImpl) httpRequest).getBody() + "\n \n" +
+                "Response Body = " + response.getBody().asString();
+        Allure.addAttachment("Request Detail", html);
         return html;
     }
 
